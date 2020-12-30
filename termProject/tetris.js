7411b0c5fdeded6fb6f02bd87569f1190d34f30d
baris <baris.kislak@stu.fsm.edu.tr>
2020-12-30 21:09:11 +0300

    tetris js updated

--- termProject/tetris.js
+++ termProject/tetris.js
@@ -1,6 +1,78 @@
 const canvas = document.getElementById('tetris');
 const context = canvas.getContext('2d');
+class Swipe {
+    constructor(element) {
+        this.xDown = null;
+        this.yDown = null;
+        this.element = typeof (element) === 'string' ? document.querySelector(element) : element;
 
+        this.element.addEventListener('touchstart', function (evt) {
+            this.xDown = evt.touches[0].clientX;
+            this.yDown = evt.touches[0].clientY;
+        }.bind(this), false);
+
+    }
+
+    onLeft(callback) {
+        this.onLeft = callback;
+
+        return this;
+    }
+
+    onRight(callback) {
+        this.onRight = callback;
+
+        return this;
+    }
+
+    onUp(callback) {
+        this.onUp = callback;
+
+        return this;
+    }
+
+    onDown(callback) {
+        this.onDown = callback;
+
+        return this;
+    }
+
+    handleTouchMove(evt) {
+        if (!this.xDown || !this.yDown) {
+            return;
+        }
+
+        var xUp = evt.touches[0].clientX;
+        var yUp = evt.touches[0].clientY;
+
+        this.xDiff = this.xDown - xUp;
+        this.yDiff = this.yDown - yUp;
+
+        if (Math.abs(this.xDiff) > Math.abs(this.yDiff)) { // Most significant.
+            if (this.xDiff > 0) {
+                this.onLeft();
+            } else {
+                this.onRight();
+            }
+        } else {
+            if (this.yDiff > 0) {
+                this.onUp();
+            } else {
+                this.onDown();
+            }
+        }
+
+        // Reset values.
+        this.xDown = null;
+        this.yDown = null;
+    }
+
+    run() {
+        this.element.addEventListener('touchmove', function (evt) {
+            this.handleTouchMove(evt);
+        }.bind(this), false);
+    }
+}
 
 context.scale(20, 20);
 
@@ -11,7 +83,24 @@ function createMatrix(w, h) {
     }
     return matrix;
 }
-function createTetrosoid(type){
+function arenaSweep() {
+    let rowCount = 1;
+    outer: for (let y = arena.length - 1; y > 0; --y) {
+        for (let x = 0; x < arena[y].length; ++x) {
+            if (arena[y][x] === 0) {
+                continue outer;
+            }
+        }
+
+        const row = arena.splice(y, 1)[0].fill(0);
+        arena.unshift(row);
+        ++y;
+
+        player.score += rowCount * 10;
+        rowCount *= 2;
+    }
+}
+function createTetrosoid(type) {
     if (type === 'I') {
         return [
             [0, 1, 0, 0],
@@ -81,15 +170,15 @@ function merge(arena, player) {
     });
 }
 
-function tetrisoidRotate(dir){
+function tetrisoidRotate(dir) {
     let offset = 1;
     rotateMatrix(player.tetrosoid, dir);
-    while(collide(arena, player)){
+    while (collide(arena, player)) {
         player.pos.x += offset;
-        offset = -(offset + (offset > 0 ? 1: -1));
-        if(offset > player.tetrosoid[0].length){
+        offset = -(offset + (offset > 0 ? 1 : -1));
+        if (offset > player.tetrosoid[0].length) {
             rotateMatrix(player.tetrosoid, -dir);
-            player.pos.x =pos;
+            player.pos.x = pos;
             return;
         }
     }
@@ -99,23 +188,24 @@ function playerReset() {
     player.tetrosoid = createTetrosoid(pieces[pieces.length * Math.random() | 0]);
     player.pos.y = 0;
     player.pos.x = (arena[0].length / 2 | 0) -
-                   (player.tetrosoid[0].length / 2 | 0);
+        (player.tetrosoid[0].length / 2 | 0);
     if (collide(arena, player)) {
         arena.forEach(row => row.fill(0));
+        alert("Game Over \nYour score : " + player.score);
         player.score = 0;
-        updateScore();
+        scoreUpdate();
     }
 }
-function rotateMatrix(matrix, dir){
+function rotateMatrix(matrix, dir) {
     for (let y = 0; y < matrix.length; ++y) {
         for (let x = 0; x < y; ++x) {
             [
                 matrix[x][y],
                 matrix[y][x],
             ] = [
-                matrix[y][x],
-                matrix[x][y],
-            ];
+                    matrix[y][x],
+                    matrix[x][y],
+                ];
         }
     }
 
@@ -126,19 +216,21 @@ function rotateMatrix(matrix, dir){
     }
 }
 
-function playerDown(){
+function playerDown() {
     player.pos.y++;
     if (collide(arena, player)) {
         player.pos.y--;
         merge(arena, player);
         player.pos.y = 0;
         playerReset();
+        arenaSweep();
+        scoreUpdate();
     }
     dropCounter = 0;
 }
-function playerRightLeft(direction){
+function playerRightLeft(direction) {
     player.pos.x += direction;
-    if(collide(arena,player)){
+    if (collide(arena, player)) {
         player.pos.x -= direction;
     }
 }
@@ -147,7 +239,7 @@ function playerRightLeft(direction){
 function draw() {
     context.fillStyle = '#000';
     context.fillRect(0, 0, canvas.width, canvas.height);
-    drawTetrisBlock(arena,{x:0, y:0});
+    drawTetrisBlock(arena, { x: 0, y: 0 });
     drawTetrisBlock(player.tetrosoid, player.pos);
 }
 
@@ -177,19 +269,27 @@ function update(time = 0) {
     requestAnimationFrame(update);
 }
 
-document.addEventListener('keydown', event => {
-    const keyName = event.key;
-    console.log(keyName);
-    if (keyName == 'ArrowRight') {
-        playerRightLeft(+1);
-    } else if (keyName == 'ArrowLeft') {
-        playerRightLeft(-1);
-    } else if (keyName == 'ArrowDown') {
-        playerDown();
-    }else if (keyName == 'ArrowUp') {
-        tetrisoidRotate();
-    }
+function scoreUpdate() {
+    document.getElementById('score').innerText = player.score;
+}
 
+document.addEventListener('keydown', event => {
+    switch (event.key) {
+        case 'ArrowRight':
+            playerRightLeft(+1);
+            break;
+        case 'ArrowLeft':
+            playerRightLeft(-1);
+            break;
+        case 'ArrowDown':
+            playerDown();
+            break;
+        case 'ArrowUp':
+            tetrisoidRotate();
+            break;
+        default:
+            break;
+    }
 }
 );
 const colors = [
@@ -206,7 +306,20 @@ const colors = [
 const player = {
     pos: { x: 5, y: 5 },
     tetrosoid: null,
+    score: 0,
 }
+var swiper = new Swipe(document.getElementById('tetris'));
+swiper.onLeft(function () {
+    playerRightLeft(-1);
+});
+swiper.onRight(function () {
+    playerRightLeft(+1);
+});
+swiper.onDown(function () { playerDown(); });
+swiper.onUp(function () { tetrisoidRotate(); });
+swiper.run();
 const arena = createMatrix(12, 20);
+
 playerReset()
+scoreUpdate();
 update();
